f6c64ad6d385c48ad96b891ca74653e8e81eefde
Johannes Schindelin <johannes.schindelin@gmx.de>
2019-11-02 20:53:54 +0100

    Adjust the script to run outside of an Azure Pipeline
    For developing the script, it is crucial that it works in a local
    checkout.
    
    So let's handle the situation where `last.modified` is not interpolated
    and pretend that it is a known-past value (rather than using a local
    script with the name `last.modified` that needs to be in the `PATH` and
    that is expected to output such a known-past Last-Modified: value).
    
    While at it, only download the tar file if it has not been downloaded
    already (and is new enough).
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- run.sh
+++ run.sh
@@ -4,7 +4,10 @@ set -ex
 
 url=https://cdist2.perforce.com/perforce/r19.1/bin.macosx1010x86_64/helix-core-server.tgz
 
-curl -I --silent --header 'If-Modified-Since: $(last.modified)' $url | tr -d '\r' >out
+test -n "$LAST_MODIFIED" ||
+LAST_MODIFIED="Fri, 11 Oct 2019 20:53:44 GMT"
+
+curl -I --silent --header "If-Modified-Since: $LAST_MODIFIED" $url | tr -d '\r' >out
 first_line=$(head -n 1 <out)
 case "$first_line" in
 "HTTP/1.1 304 Not Modified")
@@ -12,9 +15,23 @@ case "$first_line" in
 "HTTP/1.1 200 OK")
     # not up to date
     new_last_modified="$(sed -n 's/^Last-Modified: //p' <out)"
-    curl --silent -o helix-core-server.tgz $url
-    new_version="$(tar Oxvf helix-core-server.tgz Versions.txt | sed -n 's/^Rev\. P4D\/[^\/]*\/20\([^\/]*\)\/\([^ ]*\).*/\1-\2/p')"
-    new_sha256="$(openssl dgst -sha256 helix-core-server.tgz)"
+
+    file=helix-core-server.tgz
+    if test ! -f $file ||
+        case "$(uname -s)" in
+	Darwin)
+	    test $(date -j -f "%a, %d %B %Y %H:%M:%S GMT" "$new_last_modified" +%s) -gt $(stat -f %m $file)
+	    ;;
+	*)
+	    test $(date -d "$new_last_modified" +%s) -gt $(stat -c %Y $file)
+	    ;;
+	esac
+    then
+	curl --silent -o helix-core-server.tgz $url
+    fi
+
+    new_version="$(tar Oxvf $file Versions.txt | sed -n 's/^Rev\. P4D\/[^\/]*\/20\([^\/]*\)\/\([^ ]*\).*/\1-\2/p')"
+    new_sha256="$(openssl dgst -sha256 $file)"
     new_sha256="${new_sha256##* }"
     echo "TODO: automatically open a PR with $new_version and $new_sha256, and then update last.modified=$new_last_modified" >&2
     exit 1
@@ -22,11 +39,14 @@ case "$first_line" in
     # TODO: open a PR like https://github.com/Homebrew/homebrew-cask/pull/70981
 
     # Update the last.modified variable in this build definition
-    auth_header="Authorization: Bearer $SYSTEM_ACCESSTOKEN"
-    url="$SYSTEM_TEAMFOUNDATIONSERVERURI$SYSTEM_TEAMPROJECTID/_apis/build/definitions/$SYSTEM_DEFINITIONID?api-version=5.0"
-    original_json="$(curl --silent -H "$auth_header" -H "Accept: application/json; api-version=5.0; excludeUrls=true" "$url")"
-    json="$(echo "$original_json" | sed 's/\("last.modified":{"value":"\)[^"]*/\1'"$new_last_modified"/)"
-    curl --silent -X PUT -H "$auth_header" -H "Content-Type: application/json" -d "$json" "$url"
+    if test -n "$SYSTEM_ACCESSTOKEN"
+    then
+        auth_header="Authorization: Bearer $SYSTEM_ACCESSTOKEN"
+        url="$SYSTEM_TEAMFOUNDATIONSERVERURI$SYSTEM_TEAMPROJECTID/_apis/build/definitions/$SYSTEM_DEFINITIONID?api-version=5.0"
+        original_json="$(curl --silent -H "$auth_header" -H "Accept: application/json; api-version=5.0; excludeUrls=true" "$url")"
+        json="$(echo "$original_json" | sed 's/\("last.modified":{"value":"\)[^"]*/\1'"$new_last_modified"/)"
+        curl --silent -X PUT -H "$auth_header" -H "Content-Type: application/json" -d "$json" "$url"
+    fi
     ;;
 *)
     echo "Unexpected curl result:" >&2
