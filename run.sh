286529817e92cdfd2458cf6e11d986b627e05cab
Johannes Schindelin <johannes.schindelin@gmx.de>
2021-07-19 14:00:51 +0200

    Work around recent version mismatches
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- run.sh
+++ run.sh
@@ -12,10 +12,10 @@ LAST_MODIFIED="Fri, 11 Oct 2019 20:53:44 GMT"
 curl -I --silent --header "If-Modified-Since: $LAST_MODIFIED" $url | tr -d '\r' >out
 first_line=$(head -n 1 <out)
 
-if test "HTTP/1.1 404 Not Found" = "$first_line" && test 20.2 = "$rver"
+if test "HTTP/1.1 404 Not Found" = "$first_line" && test 21.2 = "$rver"
 then
 	# Seems that Perforce only offers 20.1 for public download
-	rver=20.1
+	rver=21.1
 	url=https://cdist2.perforce.com/perforce/r$rver/bin.macosx1015x86_64/helix-core-server.tgz
 	curl -I --silent --header "If-Modified-Since: $LAST_MODIFIED" $url | tr -d '\r' >out
 	first_line=$(head -n 1 <out)
